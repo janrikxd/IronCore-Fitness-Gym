016c04102338cffc8238fdca28cae8732c4672c2
janrikxd <johnrickmagcale86@gmail.com>
2026-01-14 04:45:52 +0800

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,22 +1,14 @@
 import mysql.connector
-import os
 
 def create_connection():
     try:
-
-        host = os.environ.get('MYSQLHOST', 'localhost')
-        user = os.environ.get('MYSQLUSER', 'root')
-        password = os.environ.get('MYSQLPASSWORD', '')
-        database = os.environ.get('MYSQLDATABASE', 'gym_db')
-        port = int(os.environ.get('MYSQLPORT', 3306))
-
         return mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database,
-            port=port
+            host="tramway.proxy.rlwy.net",
+            user="root",
+            password="jlLxhTwMbOKTooHQzEBBgpgtwqTIXZWg",
+            database="railway",
+            port=14890
         )
-    except mysql.connector.Error as err:
+    except Exception as err:
         print(f"Error: {err}")
         return None
