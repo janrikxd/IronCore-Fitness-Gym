909adf0df09f417360b8640a27b417cdf71b9ad3
janrikxd <johnrickmagcale86@gmail.com>
2026-01-14 04:53:52 +0800

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,14 +1,25 @@
 import mysql.connector
+import os
 
 def create_connection():
     try:
-        return mysql.connector.connect(
+        conn = mysql.connector.connect(
             host="tramway.proxy.rlwy.net",
             user="root",
             password="jlLxhTwMbOKTooHQzEBBgpgtwqTIXZWg",
             database="railway",
-            port=14890
+            port=14890,
+            
+            connect_timeout=10 
         )
-    except Exception as err:
-        print(f"Error: {err}")
+        if conn.is_connected():
+            print("Successfully connected to Railway Database!")
+            return conn
+            
+    except mysql.connector.Error as err:
+       
+        print(f"Database Error: {err}")
+        return None
+    except Exception as e:
+        print(f"General Error: {e}")
         return None
