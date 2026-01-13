b74251ccf3b49a46e48da4459aa3ecb6315565df
janrikxd <johnrickmagcale86@gmail.com>
2026-01-14 01:30:28 +0800

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,9 +1,17 @@
 import mysql.connector
+import os
 
 def create_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="gym_db"
-    )
+
+    try:
+        return mysql.connector.connect(
+            
+            host=os.environ.get('MYSQLHOST', 'localhost'),
+            user=os.environ.get('MYSQLUSER', 'root'),
+            password=os.environ.get('MYSQLPASSWORD', ''), 
+            database=os.environ.get('MYSQLDATABASE', 'gym_db'),
+            port=int(os.environ.get('MYSQLPORT', 24890))
+        )
+    except mysql.connector.Error as err:
+        print(f"Database Connection Error: {err}")
+        return None
